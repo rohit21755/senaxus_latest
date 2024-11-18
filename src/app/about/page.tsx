97aea279ac997fe269d7362b8ff9c62d3220ac3f
rohit21755 <rohit.k09022002@gmail.com>
2024-11-18 22:19:30 +0530

    new

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import AOS from "aos";
 import 'aos/dist/aos.css'
 import { useEffect } from "react";
 import Hero from "@/components/about/Hero"
-import Wcu from "@/components/about/Wcu"
+// import Wcu from "@/components/about/Wcu"
 import Team from "@/components/about/Team"
 import AircraftType from "@/components/about/AircraftType";
 export default function About(){
